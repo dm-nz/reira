dd537de5604d1cff243375fffd1e92684a47840f
dmnz <nazzzim@gmail.com>
2019-03-14 17:20:29 +0600

    Flex custom logo

--- inc/images-setup.php
+++ inc/images-setup.php
@@ -2,14 +2,15 @@
 /**
  * Register image sizes
  *
- * @package nelumbo
+ * @package reira
  */
 
 // Cover thumbnail
 add_image_size( 'cover', 1920, 1080, true );
 
 // Logo
-add_theme_support('custom-logo', array(
+add_theme_support( 'custom-logo', array(
 	'height' => 80,
-	'flex-height' => true
+	'flex-width'  => true,
+	'flex-height' => true,
 ));
