96075349616cccd28cb20246e7615a802942bba8
dmnz <nazzzim@gmail.com>
2019-05-12 15:38:06 +0600

    Fix indentation

--- inc/walkers.php
+++ inc/walkers.php
@@ -35,50 +35,50 @@ class Reira_Walker_Comment extends Walker_Comment {
 	 * @param array      $args    An array of arguments.
 	 */
 	protected function html5_comment( $comment, $depth, $args ) {
-			$tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
-?>
-			<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
-					<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
-							<footer class="comment-meta">
-									<div class="comment-author vcard">
-											<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-											<?php
-													/* translators: %s: comment author link */
-													printf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) )
-											?>
-									</div><!-- .comment-author -->
+		$tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
+		?>
+		<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
+			<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
+				<footer class="comment-meta">
+					<div class="comment-author vcard">
+						<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+						<?php
+						/* translators: %s: comment author link */
+						printf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) )
+						?>
+					</div><!-- .comment-author -->
 
-									<div class="comment-metadata">
-											<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
-													<time datetime="<?php comment_time( 'c' ); ?>">
-															<?php
-																	/* translators: 1: comment date, 2: comment time */
-																	printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() );
-															?>
-													</time>
-											</a>
-											<?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
-									</div><!-- .comment-metadata -->
+					<div class="comment-metadata">
+						<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
+							<time datetime="<?php comment_time( 'c' ); ?>">
+								<?php
+								/* translators: 1: comment date, 2: comment time */
+								printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() );
+								?>
+							</time>
+						</a>
+						<?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .comment-metadata -->
 
-									<?php if ( '0' == $comment->comment_approved ) : ?>
-									<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
-									<?php endif; ?>
-							</footer><!-- .comment-meta -->
+					<?php if ( '0' == $comment->comment_approved ) : ?>
+						<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
+					<?php endif; ?>
+				</footer><!-- .comment-meta -->
 
-							<div class="comment-content">
-									<?php comment_text(); ?>
-							</div><!-- .comment-content -->
+				<div class="comment-content">
+					<?php comment_text(); ?>
+				</div><!-- .comment-content -->
 
-							<?php
-							comment_reply_link( array_merge( $args, array(
-									'add_below' => 'div-comment',
-									'depth'     => $depth,
-									'max_depth' => $args['max_depth'],
-									'before'    => '<div class="reply">',
-									'after'     => '</div>'
-							) ) );
-							?>
-					</article><!-- .comment-body -->
-<?php
+				<?php
+				comment_reply_link( array_merge( $args, array(
+					'add_below' => 'div-comment',
+					'depth'     => $depth,
+					'max_depth' => $args['max_depth'],
+					'before'    => '<div class="reply">',
+					'after'     => '</div>'
+				) ) );
+				?>
+			</article><!-- .comment-body -->
+	<?php
 	}
 }
--- inc/widgets.php
+++ inc/widgets.php
@@ -9,13 +9,13 @@
 
 function reira_widgets_init() {
 	register_sidebar( array(
-		'name'			=> esc_html__( 'Sidebar', 'reira' ),
-		'id'			=> 'sidebar-1',
-		'description'	 => esc_html__( 'Add widgets here.', 'reira' ),
+		'name'					=> esc_html__( 'Sidebar', 'reira' ),
+		'id'						=> 'sidebar-1',
+		'description'		=> esc_html__( 'Add widgets here.', 'reira' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s large-4 medium-6 cell">',
 		'after_widget'	=> '</section>',
 		'before_title'	=> '<h2 class="widget-title h4">',
-		'after_title'	 => '</h2>',
+		'after_title'	 	=> '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'reira_widgets_init' );
